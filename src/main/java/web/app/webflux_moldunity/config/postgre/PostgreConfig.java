1e25c47e4856660c8454b71a11d77393550193ba
JSanea <josualexandru10@gmail.com>
2025-04-22 21:52:34 +0300

    postgresql config

---
+++ src/main/java/web/app/webflux_moldunity/config/postgre/PostgreConfig.java
@@ -0,0 +1,56 @@
+package web.app.webflux_moldunity.config.postgre;
+
+import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactoryOptions;
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+
+import static io.r2dbc.spi.ConnectionFactoryOptions.*;
+
+@Configuration
+public class PostgreConfig extends AbstractR2dbcConfiguration {
+    @Value("${postgresql.host}")
+    private String host;
+
+    @Value("${postgresql.port}")
+    private int port;
+
+    @Value("${postgresql.name}")
+    private String database;
+
+    @Value("${postgresql.username}")
+    private String username;
+
+    @Value("${postgresql.password}")
+    private String password;
+
+    @Override
+    @NonNull
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        return ConnectionFactories.get(
+                ConnectionFactoryOptions.builder()
+                        .option(DRIVER, "postgresql")
+                        .option(HOST, host)
+                        .option(PORT, port)
+                        .option(DATABASE, database)
+                        .option(USER, username)
+                        .option(PASSWORD, password)
+                        .build()
+        );
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/web/app/webflux_moldunity/config/security/SecurityConfig.java
@@ -0,0 +1,9 @@
+package web.app.webflux_moldunity.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+}
