12259a58c2049952efb096b106bdb59f7ac4dffa
JSanea <josualexandru10@gmail.com>
2025-04-22 20:42:45 +0300

    init

---
+++ src/main/java/web/app/webflux_moldunity/WebfluxMoldunityApplication.java
@@ -0,0 +1,11 @@
+package web.app.webflux_moldunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxMoldunityApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxMoldunityApplication.class, args);
+	}
+}
---
+++ src/test/java/web/example/webflux_moldunity/WebfluxMoldunityApplicationTests.java
@@ -0,0 +1,13 @@
+package web.example.webflux_moldunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxMoldunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
