f7226020598d11ea255441b5cc3accd25fcb489a
JSanea <josualexandru10@gmail.com>
2025-04-29 21:45:24 +0300

    updates

--- src/main/java/web/app/webflux_moldunity/repository/UserRepository.java
+++ src/main/java/web/app/webflux_moldunity/repository/UserRepository.java
@@ -2,7 +2,6 @@ package web.app.webflux_moldunity.repository;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 import web.app.webflux_moldunity.entity.user.User;
 import org.springframework.data.r2dbc.repository.Query;
@@ -10,11 +9,9 @@ import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface UserRepository extends ReactiveCrudRepository<User, Long> {
-    @Transactional(readOnly = true)
     @Query(value = "select * from users where username = :username")
     Mono<User> findByUsername(@Param("username") String username);
 
-    @Transactional(readOnly = true)
     @Query(value = "select username from users where username = :username")
-    String existUsername(@Param("username") String username);
+    Mono<String> existUsername(@Param("username") String username);
 }
